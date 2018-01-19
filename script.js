1c8841226970020fbb09619c58c13e55607025fd
Gary Goud <gary.goudsmit@hotmail.com>
2018-01-19 16:12:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
+            return bot.say('Hi! I\'m GoudBot!')
                 .then(() => 'askName');
         }
     },
@@ -22,6 +22,16 @@ module.exports = new Script({
             return bot.setProp('name', name)
                 .then(() => bot.say(`Great! I'll call you ${name}
 Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+                .then(() => 'difficultquestions');
+        }
+    },
+    
+     askName: {
+        prompt: (bot) => bot.say('Ready to ask me difficult questions?'),
+        receive: (bot, message) => {
+            const difficultquestions = message.text;
+            return bot.setProp('difficultquestions', difficultquestions)
+                .then(() => bot.say(`Great! I'm ready for that! I think we are about to live a great journey!);
                 .then(() => 'finish');
         }
     },
